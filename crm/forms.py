74fb868a862c156d1f450259649bd4c50bc0bed5
jbasyal <jbasyal@unomaha.edu>
2019-07-17 23:06:13 -0500

    added

---
+++ crm/forms.py
@@ -0,0 +1,19 @@
+from django import forms
+from .models import Customer, Service, Product
+
+class CustomerForm(forms.ModelForm):
+    class Meta:
+        model = Customer
+        fields = ('cust_name', 'organization', 'role', 'bldgroom', 'account_number', 'address', 'city', 'state', 'zipcode', 'email','phone_number')
+
+
+class ServiceForm(forms.ModelForm):
+   class Meta:
+       model = Service
+       fields = ('service_category', 'description', 'location', 'setup_time', 'cleanup_time', 'service_charge' )
+
+
+class ProductForm(forms.ModelForm):
+   class Meta:
+       model = Product
+       fields = ('product', 'p_description', 'quantity', 'pickup_time', 'charge' )
